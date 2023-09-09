ed6013a804bfb9f21b9fdef4f3d89403e6b6d5db
saifsaeed48538 <142862451+saifsaeed48538@users.noreply.github.com>
2023-09-09 09:23:26 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -21,18 +21,18 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownernomer = "916909137213" //ur owner number2
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.owner = ['923287893664'] //ur owner number
+global.ownernomer = "923287893664" //ur owner number2
+global.ownername = "🇵🇰𝙎𝙖𝙞𝙛 𝙎𝙖𝙚𝙚𝙙" //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.socialm = "GitHub:saifsaeed48538" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //new
-global.botname = "Cheems Bot MD V8"
-global.ownernumber = '916909137213'
-global.ownername = '🦄드림 가이 Xeon'
-global.ownerNumber = ["916909137213@s.whatsapp.net"]
+global.botname = "𝙎𝙖𝙞𝙛 𝙈𝙙 𝘽𝙤𝙩 "
+global.ownernumber = '923287893664'
+global.ownername = '🇵🇰𝙎𝙖𝙞𝙛 𝙎𝙖𝙚𝙚𝙙 𝘽𝙤𝙩'
+global.ownerNumber = ["923287893664@s.whatsapp.net"]
 global.ownerweb = "https://youtube.com/@DGXeon"
 global.websitex = "https://youtu.be/L_SIk59QeAU"
 global.wagc = "https://chat.whatsapp.com/Dc2qyVeK8JbJq8Gr3U1pKH"
